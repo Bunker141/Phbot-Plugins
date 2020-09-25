cebe260704fc0259dfafd6d0a6fb99d7b0335661
Bunker141 <67886612+Bunker141@users.noreply.github.com>
2020-09-25 00:27:14 -0400

    Add files via upload

---
+++ Mount.py
@@ -0,0 +1,35 @@
+from phBot import *
+from threading import Timer
+import QtBind
+import struct
+
+
+Joined = False
+
+gui = QtBind.init(__name__, 'Mount')
+
+
+button1 = QtBind.createButton(gui, 'button_clicked', ' Mount ', 10, 10)
+
+
+def button_clicked():
+	mount() 
+
+def mount():
+	pets = get_pets()
+	if pets:
+		for uid,pet in pets.items():
+			if pet['type'] == "transport":
+				p = b'\x01'
+				p += struct.pack('I',uid)
+				log('Mounting... ;)')
+				inject_joymax(0x70CB,p, False)
+
+
+
+
+def teleported():
+	Timer(1.0, mount, ()).start()
+
+
+log('[%s] Loaded' % __name__)
